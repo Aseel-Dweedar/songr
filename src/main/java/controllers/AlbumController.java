606f46442506757ac130e1b271ac7684592ffbd4
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-08-12 00:37:10 +0300

    lab 13 -401

--- src/main/java/controllers/AlbumController.java
+++ src/main/java/controllers/AlbumController.java
@@ -1,18 +1,25 @@
 package controllers;
 
 import com.example.songr.models.Album;
+import com.example.songr.models.Song;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import repository.AlbumsRepository;
+import repository.SongRepository;
+
+import java.util.List;
 
 @Controller
 public class AlbumController {
 
     @Autowired
     AlbumsRepository albumsRepository;
+
+    @Autowired
+    SongRepository songRepository;
 
     @GetMapping("/albums")
     public String getAlbumList(Model model) {
@@ -25,5 +32,14 @@ public class AlbumController {
         model.addAttribute("album" , album);
         albumsRepository.save(album);
         return new RedirectView("/albums");
+    }
+
+    @GetMapping("/oneAlbum")
+    public String getOneAlbum(@RequestParam Integer id , Model model){
+        Album albumToRender = albumsRepository.findById(id).get();
+        model.addAttribute("album" , albumToRender);
+        List<Song> albumSongs = songRepository.findAllByAlbum(albumToRender);
+        model.addAttribute("songs" , albumSongs);
+        return "oneAlbum.html";
     }
 }
---
+++ src/main/java/controllers/SongController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import com.example.songr.models.Album;
+import com.example.songr.models.Song;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+import repository.AlbumsRepository;
+import repository.SongRepository;
+
+@Controller
+public class SongController {
+
+    @Autowired
+    SongRepository songRepository;
+
+    @Autowired
+    AlbumsRepository albumsRepository;
+
+
+    @GetMapping("/song")
+    public String getAllSongs(Model model) {
+        model.addAttribute("song" , songRepository.findAll());
+        return "song.html";
+    }
+
+    @PostMapping("/addsong")
+    public RedirectView addSong(@RequestParam String title, Integer length , Integer  trackNumber , Integer id , Model model){
+        Album songAlbum = albumsRepository.findById(id).get();
+        Song addNewSong = new Song(title , length , trackNumber , songAlbum);
+        songRepository.save(addNewSong);
+        return new RedirectView("/oneAlbum?id="+id) ;
+    }
+}
